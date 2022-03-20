d07591a10d9b70cf8ca7ead573e41ccb3899349a
camariana <camariana40@outlook.com>
2022-03-20 15:01:57 +0000

    text app with initial data of the blogs

---
+++ fs-part-7-bloglist-front-end/src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+
+const App = () => {
+
+  // const blogs = useSelector( state => state.blogs )
+  // console.log(blogs);
+
+
+  return (
+    <div>
+       <p>
+        Peace
+      </p>
+    </div>
+  )
+}
+
+export default App
