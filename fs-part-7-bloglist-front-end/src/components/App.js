9d8b56cefba41974ed61579c742c27be372fae47
camariana <camariana40@outlook.com>
2022-03-21 10:54:42 +0000

    update the folder name

--- fs-part-7-bloglist-front-end/src/components/App.js
+++ fs-part-7-bloglist-front-end/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { initialBlogs } from '../redux/features/blogs/blogsSlice'
+import { initialBlogs } from '../redux/reducers/blogs/blogsSlice'
 
 const App = () => {
   const dispatch = useDispatch()
