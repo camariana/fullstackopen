1cefa0f70504efc6d8c834c8e07cb45e75d63f43
camariana <camariana40@outlook.com>
2022-04-03 16:30:12 +0000

    add routes

--- fs-part-7/bloglist-front-end/src/components/App.js
+++ fs-part-7/bloglist-front-end/src/components/App.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {
   BrowserRouter as Router,
@@ -11,24 +11,32 @@ import { initialBlogs } from '../redux/reducers/blogs/blogsSlice'
 import Header from './Header/Header'
 import Blogs from './Blog/Blogs'
 import Blog from './Blog/Blog'
+import Login from './Login/Login'
 
 
 const App = () => {
   const dispatch = useDispatch()
   const blogs = useSelector( state => state.blogs )
+  const user = useSelector( state => state.user )
 
   useEffect(() => {
-    dispatch(initialBlogs)
+    dispatch(initialBlogs())
   }, [dispatch])
+  
 
   return (
     <div className='page'>
-      <Header />
+      <Header user={user} />
       <Routes>
-        <Route path='/' element={<Blogs blogs={blogs} />} />
+       
         
         <Route path='/blogs' element={<Blogs blogs={blogs} />} />
         <Route path='/blogs/:id' element={<Blog blogs={blogs} />} />
+
+
+        <Route path='/login' element={<Login user={user} />} />
+
+        <Route path='/' element={<Blogs blogs={blogs} />} />
       </Routes>
     </div>
   )
