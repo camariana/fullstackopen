0c27174fd012eecaee2efd283ace66573944e03c
camariana <camariana40@outlook.com>
2022-04-11 14:58:15 +0000

    add button compoenents

---
+++ fs-part-7/bloglist-front-end/src/components/Buttons/DeleteButton.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { deleteBlog } from '../../redux/reducers/blogs/blogsSlice'
+
+
+const DeleteButton = ({ id }) => {
+
+  const dispatch = useDispatch()
+  const blogs = useSelector( state => state.blogs )
+  
+  const remove = id => {
+    const deletedBlog = blogs.find((blog) => blog.id === id)
+    
+    const yes = window
+      .confirm(`Are you sure you want to delete ${deletedBlog.title} by ${deletedBlog.author}`)
+
+    if (!yes) {
+      return
+    }
+
+    dispatch(deleteBlog(deletedBlog))
+  }
+
+
+  return (
+    <button className='button' onClick={() => remove(id)}>
+      Delete
+    </button>
+  )
+}
+
+export default DeleteButton
