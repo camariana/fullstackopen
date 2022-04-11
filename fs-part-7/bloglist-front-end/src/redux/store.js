528fd05f5b724931e2d7294bd91f4e27b8ecf947
camariana <camariana40@outlook.com>
2022-04-11 15:01:43 +0000

    add notification reducer

--- fs-part-7/bloglist-front-end/src/redux/store.js
+++ fs-part-7/bloglist-front-end/src/redux/store.js
@@ -3,12 +3,16 @@ import { configureStore } from '@reduxjs/toolkit'
 import blogReducer from './reducers/blogs/blogsSlice'
 import userReducer from './reducers/users/userSlice'
 import usersReducer from './reducers/users/usersSlice'
+import notificationReducer from './reducers/notification/notificationSlice'
+
+
 
 const store = configureStore({
   reducer: {
     blogs: blogReducer,
     users: usersReducer,
-    user: userReducer
+    user: userReducer,
+    notification: notificationReducer
   }
 })
 
