7c94394ec337be83677778512fdcb8c4f64ce4c8
camariana <camariana40@outlook.com>
2022-04-15 22:22:55 +0000

    take button to its own component

--- fs-part-7/bloglist-front-end/src/components/_includes/User.jsx
+++ fs-part-7/bloglist-front-end/src/components/_includes/User.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import SignoutButton from '../Buttons/SignoutButton'
 
 const User = ({ user }) => {
   return (
@@ -10,15 +11,8 @@ const User = ({ user }) => {
             <span>
               { user }
             </span>
-            <svg 
-              xmlns="http://www.w3.org/2000/svg" 
-              width="16" 
-              height="16" 
-              className='icon'
-              viewBox="0 0 16 16">
-              <path d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
-              <path d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
-            </svg>
+            <SignoutButton />
+            
           </div>
         : <Link className='button button--link' to={'/login'}>
             Login
