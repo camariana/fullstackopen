12fadc4054f27ab28da6c879007a6dabfbc3e119
camariana <camariana40@outlook.com>
2022-04-28 11:05:54 +0000

    add masthead for the home page

---
+++ fs-part-7/bloglist-front-end/src/components/Masthead/Masthead.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Masthead = () => {
+  return (
+    <div className='masthead' role='banner'>
+      <div className="avatar">
+        <span>AC</span>
+      </div>
+      <h1>Peace, I'm A Camariana</h1>
+      <p>
+        This is where I blog about interesting things on the web and my experience learning Full Stack Open (React, Redux, Node.js, MongoDB, GraphQL and TypeScript in one go!). 
+      </p>
+
+      <blockquote>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore beatae ea mollitia deleniti praesentium blanditiis fugiat facere cupiditate ipsam, quas cumque vitae nesciunt aut doloremque velit tenetur aperiam amet possimus.
+        </p>
+
+        <cite>Matti Lukuuni</cite>
+
+      </blockquote>
+    </div>
+  )
+}
+
+export default Masthead
