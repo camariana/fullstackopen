c585142b7989f66ccc503aec9b500cacd551d5f8
camariana <camariana40@outlook.com>
2022-04-28 11:09:24 +0000

    remove unecessary code

--- fs-part-7/bloglist-front-end/src/index.js
+++ fs-part-7/bloglist-front-end/src/index.js
@@ -4,9 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom"
 import { Provider } from 'react-redux'
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-
 import store from './redux/store'
 
 ReactDOM.render(
@@ -16,9 +13,5 @@ ReactDOM.render(
     </Router>
   </Provider>,
   document.getElementById('root')
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
