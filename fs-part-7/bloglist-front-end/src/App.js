7d9ab3d455ece926ce17a33cbdd978f07f0ca0df
camariana <camariana40@outlook.com>
2022-04-28 11:09:05 +0000

    clean up app and add more components

--- fs-part-7/bloglist-front-end/src/App.js
+++ fs-part-7/bloglist-front-end/src/App.js
@@ -1,12 +1,6 @@
 
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import {
-  BrowserRouter as Router,
-  Routes, 
-  Route, 
-  Link
-} from "react-router-dom"
+import React from 'react'
+import { Routes, Route } from "react-router-dom"
 
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
@@ -16,27 +10,28 @@ import Blogs from './views/blog/Blogs'
 import Blog from './views/blog/Blog'
 import Users from './views/user/Users'
 import User from './views/user/User'
+import Login from './views/login/Login'
 
 
 const App = () => {
-  const notification = useSelector( state => state.notification)
-
-
   return (
     <div className='page'>
+      
       <Header />
 
-      <Notification notification={notification} />
+      <Notification />
+      
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          
+          <Route path='/blogs/:id' element={<Blog />} />
+          <Route path='/blogs' element={<Blogs />} />
+          
+          <Route path='/users/:id' element={<User />} />
+          <Route path='/users' element={<Users />} />
 
-      <Routes>
-        <Route path='/blogs/:id' element={<Blog />} />
-        <Route path='/blogs' element={<Blogs />} />
-        
-        <Route path='/users/:id' element={<User />} />
-        <Route path='/users' element={<Users />} />
-
-        <Route path='/' element={<Home />} />
-      </Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
 
       <Footer />
     </div>
