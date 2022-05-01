c8930f31cde434f82b68c294289b354a1fda09bd
camariana <camariana40@outlook.com>
2022-05-01 12:31:22 +0000

    add home page section for the lastest log

--- fs-part-7/bloglist-front-end/src/views/Home.js
+++ fs-part-7/bloglist-front-end/src/views/Home.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import NewBlogButton from '../components/Buttons/NewBlogButton'
+
 import Masthead from '../components/Masthead/Masthead'
 
 const Home = () => {
@@ -6,8 +9,22 @@ const Home = () => {
     <>
       <Masthead />
       <main>
-        The home page
+        <section className='sec'>
+          <div className='contain'>
+            <header className='sec__header'>
+              <h2>
+                Latest Bloglist
+              </h2>
+              <div>
+              <Link className='button button--secondary' to={'/blogs'}>
+                See all Blogs
+              </Link>
+              </div>
+            </header>
+          </div>
+        </section>
       </main>
+      <NewBlogButton />
     </>
   )
 }
