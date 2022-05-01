1a9e5836d1ad7d6d2ea811f86c92e66d71b4663c
camariana <camariana40@outlook.com>
2022-05-01 12:34:01 +0000

    add css

--- fs-part-7/bloglist-front-end/src/index.js
+++ fs-part-7/bloglist-front-end/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom"
 import { Provider } from 'react-redux'
 
+import './App.css'
 import App from './App';
 import store from './redux/store'
 
