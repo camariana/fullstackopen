434abcd822911691822d8d523725333c75a8cb64
camariana <camariana40@outlook.com>
2022-05-09 15:03:59 +0000

    blog list section on the home page

--- fs-part-7/bloglist-front-end/src/views/Home.js
+++ fs-part-7/bloglist-front-end/src/views/Home.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+
+import DeleteButton from '../components/Buttons/DeleteButton'
+import LikeButton from '../components/Buttons/LikeButton'
 import NewBlogButton from '../components/Buttons/NewBlogButton'
 
 import Masthead from '../components/Masthead/Masthead'
+import { getBlogs, selectBlogs } from '../redux/reducers/blogs/blogsSlice'
 
 const Home = () => {
+  const dispatch = useDispatch()
+  const blogs = useSelector(selectBlogs)
+
+  useEffect(() => {
+    dispatch(getBlogs())
+  }, [dispatch])
+
   return (
     <>
       <Masthead />
@@ -13,7 +25,7 @@ const Home = () => {
           <div className='contain'>
             <header className='sec__header'>
               <h2>
-                Latest Bloglist
+                Most Like Blogs
               </h2>
               <div>
               <Link className='button button--secondary' to={'/blogs'}>
@@ -21,6 +33,29 @@ const Home = () => {
               </Link>
               </div>
             </header>
+
+            <ul className='list'>
+            {
+              blogs.map(blog =>
+                <li className='list__item' key={blog.id}>
+                    <div className='list__primary'>
+                      <h3 className='list__title'>
+                        <Link to={`/blogs/${blog.id}`}>
+                          {blog.title}
+                        </Link>
+                      </h3>
+                      <span>
+                        Likes {blog.likes}
+                      </span>
+                    </div>
+                    <div className='list__secondary'>
+                      <LikeButton id={blog.id} />
+                      <DeleteButton id={blog.id} />
+                    </div>
+                </li>          
+              )
+            }
+            </ul>
           </div>
         </section>
       </main>
