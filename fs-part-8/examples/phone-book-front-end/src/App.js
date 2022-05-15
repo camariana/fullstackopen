9b5c950a88946f03112e5057b443211c7c48d569
camariana <camariana40@outlook.com>
2022-05-15 11:42:19 +0000

    add more components

--- fs-part-8/examples/phone-book-front-end/src/App.js
+++ fs-part-8/examples/phone-book-front-end/src/App.js
@@ -1,5 +1,8 @@
 import { gql, useQuery } from '@apollo/client'
+import { useState } from 'react'
+import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import PhoneForm from './components/PhoneForm'
 
 const ALL_PERSONS = gql`
 query {
@@ -11,16 +14,41 @@ query {
 }
 `
 
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null
+  }
+
+  return (
+    <div style={{color: 'red'}}>    
+      {errorMessage}
+    </div>
+  )
+}
+
 const App = () => {
+  const [errorMessage, setErrorMessage] =useState(null)
+
   const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
+
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
     </div>
   )
 }
